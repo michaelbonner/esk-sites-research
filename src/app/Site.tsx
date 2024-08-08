6665cf4e2c4da6140ebe849cb8b5b227e46a9357
Michael Bonner <michaelwbonner@gmail.com>
2024-08-07 23:46:30 -0600

    fix react style prop

--- src/app/Site.tsx
+++ src/app/Site.tsx
@@ -23,7 +23,7 @@ export const Site = ({ site }: { site: (typeof sites)[0] }) => {
           <svg
             stroke="currentColor"
             fill="currentColor"
-            stroke-width="0"
+            strokeWidth="0"
             viewBox="0 0 512 512"
             height="1em"
             width="1em"
